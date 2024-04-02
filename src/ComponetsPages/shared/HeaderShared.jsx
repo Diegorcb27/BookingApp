923cb2c1233d02abb093557aa1d5ed1df56fa73c
Diego Castro <diegorbcastro@gmail.com>
2024-04-01 23:02:05 -0400

    BookingApp_update_2.0

--- src/ComponetsPages/shared/HeaderShared.jsx
+++ src/ComponetsPages/shared/HeaderShared.jsx
@@ -10,6 +10,7 @@ const HeaderShared = () => {
                 <li><Link to='/reservations'>Reservations</Link></li>
                 <li><Link to='/register'>Register</Link></li>
                 <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/welcome'>Profile</Link></li>
             </ul>
         </nav>
     </header>
